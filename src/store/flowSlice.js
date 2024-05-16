c5c51eaac8126e878365f122fef06f4515974946
Mayank Mishra <maymishraank@gmail.com>
2024-05-16 23:17:23 +0530

    completed reducers and actions

--- src/store/flowSlice.js
+++ src/store/flowSlice.js
@@ -38,12 +38,51 @@ const flowSlice = createSlice({
   name: "flow",
   initialState: initialState,
   reducers: {
-    addNode: (state, action) => {},
-    addEdge: (state, action) => {},
-    selectNode: (state, action) => {},
-    unselectNode: (state, action) => {},
+    addNode: (state, action) => {
+      state.nodes = [...state.nodes, action.payload];
+    },
+    removeNode: (state, action) => {
+      state.nodes = state.nodes.filter((node) => node.id !== action.payload);
+    },
+
+    addEdge: (state, action) => {
+      state.edges = [...state.edges, action.payload];
+    },
+    removeEdge: (state, action) => {
+      state.edges = state.edges.filter((edge) => edge.id !== action.payload);
+    },
+
+    nodeChanges: (state, action) => {
+      state.nodes = action.payload;
+    },
+    edgeChanges: (state, action) => {
+      state.edges = action.payload;
+    },
+
+    selectNode: (state, action) => {
+      const selectedNodeId = action.payload;
+      const node = state.nodes?.find((node) => node.id === selectedNodeId);
+      if (node) {
+        state.selectedNode = node;
+      } else {
+        state.selectedNode = null;
+      }
+    },
+
+    unselectNode: (state) => {
+      state.selectedNode = null;
+    },
   },
 });
 
-export const { addNode, addEdge, selectNode, unselectNode } = flowSlice.actions;
+export const {
+  addNode,
+  removeNode,
+  addEdge,
+  removeEdge,
+  nodeChanges,
+  edgeChanges,
+  selectNode,
+  unselectNode,
+} = flowSlice.actions;
 export default flowSlice.reducer;
