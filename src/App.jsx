bc3aec5cba282833b2312900774acca5842a3ea8
Mayank Mishra <maymishraank@gmail.com>
2024-05-16 19:57:08 +0530

    added static nodes panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
+
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 import TopBar from "./components/TopBar";
 import SettingsPanel from "./components/SettingsPanel";
+import NodesPanel from "./components/NodesPanel";
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
   return (
     <Box sx={{ padding: "0" }}>
       <TopBar onSaveChanges={() => {}} />
       <Box sx={{ display: "flex" }}>
-        <Box sx={{ width: "75vw" }}>Flow Chart</Box>
+        <Box
+          sx={{ width: "75vw" }}
+          onDrop={(e) => {
+            const messageType = e.dataTransfer.getData("messageType");
+            console.log("messageType", messageType);
+            setMessages((prevMessages) => [...prevMessages, messageType]);
+            console.log("drop");
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+        >
+          {messages?.map((message, index) => (
+            <Box key={index} sx={{ marginBottom: "10px" }}>
+              {message}
+            </Box>
+          ))}
+        </Box>
         <Box sx={{ width: "25vw" }}>
-          <SettingsPanel />
+          <NodesPanel />
         </Box>
       </Box>
     </Box>
---
+++ src/components/NodesPanel.jsx
@@ -0,0 +1,63 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import Typography from "@mui/material/Typography";
+
+export default function NodesPanel() {
+  return (
+    <Box
+      sx={{
+        boxShadow: "-2px 0px 5px 0px rgba(0,0,0,0.2)",
+        width: "100%",
+        height: "92vh",
+        padding: "16px 8px",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Box
+            draggable
+            onDragStart={(e) => {
+              e.dataTransfer.setData("messageType", "text");
+            }}
+            sx={{
+              height: "100px",
+              borderRadius: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              border: "1px solid rgba(25, 118, 210, 0.5)",
+              cursor: "pointer",
+              "&:hover": {
+                border: "1px solid #1976d2",
+              },
+            }}
+          >
+            <MessageOutlinedIcon
+              sx={{ color: "#1976d2", marginBottom: "6px" }}
+            />
+            <Typography color="primary">Message</Typography>
+          </Box>
+        </Grid>
+
+        {/* <Grid item xs={6}>
+          <Box
+            sx={{
+              height: "100px",
+              borderRadius: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              border: "1px solid rgba(25, 118, 210, 0.5)",
+            }}
+          >
+            <MessageOutlinedIcon sx={{ color: "#1976d2" }} />
+            <Typography color="primary">Message</Typography>
+          </Box>
+        </Grid> */}
+      </Grid>
+    </Box>
+  );
+}
